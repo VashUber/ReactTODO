30e3bec4bed1a16fd30c6faf37f6d18446d5382c
Egor Sizov <mrjarokvk@gmail.com>
2021-08-25 16:47:26 +0300

    init

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const TestWrapper = styled.div`
+  color: #fff;
+  background: #000;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<TestWrapper>
+				1234
+			</TestWrapper>
+			<div>
+				1234
+			</div>
+		</div>
+
+	);
 }
 
 export default App;
